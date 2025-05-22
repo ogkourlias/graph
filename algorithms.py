d24a57997b7864eac278c93ec800ab6eb0ffa504
ogkourlias <orfeasgkourlias@gmail.com>
2025-05-22 18:08:56 +0200

    Added dijkstra's algorithm and subsequent testing against networkx

--- algorithms.py
+++ algorithms.py
@@ -14,7 +14,9 @@ __version__ = "0.1"
 import sys
 from queue import Queue
 import networkx as nx
-
+import numpy as np
+from collections import deque
+import heapq
 
 # FUNCTIONS
 def kahn(network):
@@ -76,7 +78,87 @@ def bfs(network, root):
 
     return search_order
 
+def dijkstra_bfs(network, start, end):
+    queue = Queue()
+    # Set total distance to all nodes to inf
+    nx.set_node_attributes(network, np.inf, name = "shortest")
+    nx.set_node_attributes(network, None, name="prev")
 
+    # Starting node/most accesible node distance to 0 so we start there
+    nx.set_node_attributes(network, {start:0}, "shortest")
+    
+    # {network.nodes[start]["dist"]}
+    print(f"Finding shortest path from {start} to {end}")
+    visited = set()
+    queue.put(start)
+    while not queue.empty():
+        curr_node = queue.get()
+        for neighbor in nx.neighbors(network, curr_node):
+            length = network[curr_node][neighbor]["length"]
+            new = network.nodes[curr_node]["shortest"] + length
+            if new < network.nodes[neighbor]["shortest"]:
+                network.nodes[neighbor]["shortest"] = new
+                network.nodes[neighbor]["prev"] = curr_node
+                queue.put(neighbor)
+            if neighbor not in visited:
+                queue.put(neighbor)
+        visited.add(curr_node)
+
+    if np.isnan(network.nodes[end]["prev"]) and start != end:
+        return []  # No path found
+    
+    path = deque()
+    while end != start:
+        path.appendleft(end)
+        prev = network.nodes[end]["prev"]
+        end = prev
+    path.appendleft(start)
+    return list(path)
+    
+def dijkstra(network, start, end):
+    queue = []
+    # Set total distance to all nodes to inf
+    nx.set_node_attributes(network, np.inf, name = "shortest")
+    nx.set_node_attributes(network, None, name="prev")
+
+    # Starting node/most accesible node distance to 0 so we start there
+    nx.set_node_attributes(network, {start:0}, "shortest")
+    
+    # {network.nodes[start]["dist"]}
+    print(f"Finding shortest path from {start} to {end}")
+    visited = set()
+    heapq.heappush(queue, (network.nodes[start]["shortest"], start))
+    while queue:
+        curr_dist, curr_node = heapq.heappop(queue)
+        if curr_node in visited:
+            continue
+        visited.add(curr_node)
+        if curr_node == end:
+            break
+        # Check neighbors of node
+        for neighbor in nx.neighbors(network, curr_node):
+            length = network[curr_node][neighbor]["length"]
+            new = curr_dist + length
+            if new < network.nodes[neighbor]["shortest"]:
+                network.nodes[neighbor]["shortest"] = new
+                network.nodes[neighbor]["prev"] = curr_node
+                heapq.heappush(queue, (new, neighbor))
+                # print(neighbor, new, network.nodes[neighbor]["prev"])
+            # if neighbor not in visited:
+            #     queue.put(neighbor)
+    
+    # Path impossible
+    if network.nodes[end]["prev"] is None and start != end:
+        return []
+    
+    path = deque()
+    while end != start:
+        path.appendleft(end)
+        prev = network.nodes[end]["prev"]
+        end = prev
+    path.appendleft(start)
+    return list(path)
+    
 # MAIN
 def main(args):
     """Main function"""
